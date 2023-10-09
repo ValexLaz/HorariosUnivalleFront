a94d0d67ab1af5d110262db92314d426e6c945a6
Omar Vallejos Peña <omalevp@hotmail.com>
2023-10-09 01:26:24 -0400

    formularios: semestre, facultad, carrera

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -1,8 +1,23 @@
 const router = require("express").Router();
 
 router.get('/dashboard',(req,res)=>{
-    res.render('index2.ejs')
-}) 
+    res.render('dashboard.ejs')
+})
 
+router.get('/usuarioFormulario',(req,res)=>{
+    res.render('usuarioFormulario.ejs')
+})
+
+router.get('/carreraFormulario',(req,res)=>{
+    res.render('carreraFormulario.ejs')
+})
+
+router.get('/semestreFormulario',(req,res)=>{
+    res.render('semestreFormulario.ejs')
+})
+
+router.get('/facultadFormulario',(req,res)=>{
+    res.render('facultadFormulario.ejs')
+})
 
 module.exports = router;
