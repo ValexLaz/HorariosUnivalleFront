43628b227ef6e3a5f33858033976ba1491551499
Omar Vallejos Peña <omalevp@hotmail.com>
2023-11-08 11:18:47 -0400

    Comit para empezar a hacer los formularios

--- public/js/RepositoryAPI.js
+++ public/js/RepositoryAPI.js
@@ -44,4 +44,5 @@ class RepositoryAPI {
     }
   }
 }
+
 export default RepositoryAPI;
--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -3,20 +3,16 @@ const router = require("express").Router();
 router.get('/dashboard',(req,res)=>{
     res.render('main.ejs')
 })
-
 router.get('/usuarioFormulario',(req,res)=>{
     
     res.render('usuarioFormulario.ejs')
 })
-
 router.get('/carreraFormulario',(req,res)=>{
     res.render('carreraFormulario.ejs')
 })
-
 router.get('/semestreFormulario',(req,res)=>{
     res.render('semestreFormulario.ejs')
 })
-
 router.get('/facultadFormulario',(req,res)=>{
     res.render('facultadFormulario.ejs')
 })
