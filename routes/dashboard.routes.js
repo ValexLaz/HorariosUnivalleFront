f9412669cf3133095ae30c84ead966c540a10336
Omar Vallejos Peña <omalevp@hotmail.com>
2023-11-09 22:36:53 -0400

    aula, disponibilidad, tipoAula, formularios

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -3,19 +3,6 @@ const router = require("express").Router();
 router.get('/dashboard',(req,res)=>{
     res.render('main.ejs')
 })
-<<<<<<< HEAD
-router.get('/usuarioFormulario',(req,res)=>{
-    
-    res.render('usuarioFormulario.ejs')
-})
-router.get('/carreraFormulario',(req,res)=>{
-    res.render('carreraFormulario.ejs')
-})
-router.get('/semestreFormulario',(req,res)=>{
-    res.render('semestreFormulario.ejs')
-})
-router.get('/facultadFormulario',(req,res)=>{
-=======
 
 router.get('/userForm',(req,res)=>{
     
@@ -31,7 +18,6 @@ router.get('/semesterForm',(req,res)=>{
 })
 
 router.get('/facultyForm',(req,res)=>{
->>>>>>> bf430631c6bc33c20b7c5d29b0a7d91303f3c690
     res.render('facultadFormulario.ejs')
 })
 
